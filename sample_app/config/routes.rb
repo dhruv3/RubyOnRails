784a694b6d051fe61bde94a7f8cf18223aaaf2ea
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-06-17 22:33:07 -0400

    Creating static page controller with actions home and help

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages_controller/home'
+
+  get 'static_pages_controller/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+  get 'static_pages_controller/home'
+
+  get 'static_pages_controller/help'
+
+  	root 'application#hello'
+  end
+  
 end
