f707e9f2c6e561d1eb5b19d3d84d30a732dfb4e8
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-06-18 00:14:50 -0400

    1. Updating controller name 2. New about page added 3. Test case used

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages_controller/home'
-
-  get 'static_pages_controller/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  Rails.application.routes.draw do
-  get 'static_pages_controller/home'
-
-  get 'static_pages_controller/help'
-
-  	root 'application#hello'
-  end
-  
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  root 'application#hello'
 end
