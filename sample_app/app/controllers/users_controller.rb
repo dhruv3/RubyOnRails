09c937032dd53ea3ea45daafe599353d5d6243f8
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-06-21 23:22:17 -0400

    Updated till ch-10

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  #before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   #https://stackoverflow.com/questions/6885990/rails-params-explained
   def show
     @user = User.find(params[:id])
@@ -7,4 +14,52 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
+    # Confirms a logged-in user.
+    # we restrict the filter to act only on the :edit and :update actions by passing the appropriate only: options hash.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
 end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates(:name, presence: true, length:{maximum: 50})
   
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  
 end
