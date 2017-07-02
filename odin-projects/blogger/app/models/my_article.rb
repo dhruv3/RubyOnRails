3a55ec8a235ff45219e23213a9de38b22ecb9a31
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-07-02 16:14:31 -0400

    Setting up the blog

--- odin-projects/blogger/app/models/my_article.rb
+++
@@ -1,2 +0,0 @@
-class MyArticle < ActiveRecord::Base
-end
--- odin-projects/blogger/db/schema.rb
+++ odin-projects/blogger/db/schema.rb
@@ -11,21 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629132548) do
+ActiveRecord::Schema.define(version: 20170702163912) do
 
   create_table "articles", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "blogs", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "my_articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
