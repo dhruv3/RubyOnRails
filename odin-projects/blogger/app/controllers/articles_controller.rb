6d022bf72be7096b8ba456a7e6c01beeec3d530f
Dhruv Dogra <dhruv.mnit@gmail.com>
2017-07-03 00:10:36 -0400

    Completing phase 2

--- odin-projects/blogger/app/controllers/articles_controller.rb
+++ odin-projects/blogger/app/controllers/articles_controller.rb
@@ -1,9 +1,43 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    #fail: helps to see what params we have received
+    #@article = Article.new(article_params): this is a way to use helper methods
+    @article = Article.new
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+    #we don't need to show a new page, so e redirect it to
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to action: 'index'
   end
 end
